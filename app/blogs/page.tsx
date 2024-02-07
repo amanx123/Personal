a770d01051245a8085cddd9c86a8c15d9bfc3e9e
aman <amanagarwalx123@gmail.com>
2024-02-07 22:36:44 +0530

    modified metadata

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -5,7 +5,7 @@ export default function Blogs() {
     return (
         <div >
             <h1 className="h-20 bg-black z-10 bg-opacity-90 sticky top-0 mt-6 justify-center items-center flex font-bold text-3xl px-10">BLOGS</h1>
-            <section className="container mx-auto p-8 justify-center items-center flex flex-col gap-4">
+            <section className="container mx-auto p-8 justify-center items-center flex flex-col gap-4 text-white">
                 {
                     blogs.map((blog, idx) => (
                         <Blog key={idx} title={blog.title} link={blog.link} />
